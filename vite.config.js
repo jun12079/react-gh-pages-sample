1288d367809f569ee5ffaeead4e635e57746a1a6
jun12079 <jeffery851007@gmail.com>
2024-12-16 23:01:08 +0800

    update vite setting

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //開發中npm run dev、產品路徑 npm run build
+  base: process.env.NODE_ENV === 'production' ? '/react-gh-pages-sample/' : '/',
   plugins: [react()],
 })
